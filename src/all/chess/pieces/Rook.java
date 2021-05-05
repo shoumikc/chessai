977ab567fb07f34f4fd8084cd3a80ef0179a98cf
Shoumik Chaudhuri <shoumik.c@berkeley.edu>
2021-05-05 14:30:02 -0700

    finished Rook and Bishop

--- src/all/chess/pieces/Rook.java
+++ src/all/chess/pieces/Rook.java
@@ -3,44 +3,43 @@ package all.chess.pieces;
 import all.chess.Color;
 import all.chess.board.*;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 
-public class Bishop extends Piece {
-    Bishop(int[] coordinate, Color team) {
+public class Rook extends Piece{
+    Rook(int[] coordinate, Color team) {
         super(coordinate, team);
     }
 
     @Override
     public HashSet<Move> legalMoves(Board board) {
         HashSet<Move> moves = new HashSet<>();
-        diagonalTraverser(moves, board, -1, 1); // topright
-        diagonalTraverser(moves, board, -1, -1); // topleft
-        diagonalTraverser(moves, board, 1, 1); // bottomright
-        diagonalTraverser(moves, board, 1, -1); //bottomleft
+        horizontalTraverser(moves,board,1,0); // down
+        horizontalTraverser(moves, board, -1, 0); // up
+        horizontalTraverser(moves, board, 0, 1); // right
+        horizontalTraverser(moves, board, 0,-1); // left
         return moves;
     }
-    /** Helper method for Bishop that traverses all 4 diagonals */
-    private void diagonalTraverser(HashSet<Move> set, Board board, int rowOffset, int colOffset) {
+    /** Helper method for Rook that traverses all 4 horizontals */
+    private void horizontalTraverser(HashSet<Move> set, Board board, int rowOffset, int colOffset){
         int newRow = row + rowOffset;
         int newCol = col + colOffset;
-        while (BoardUtils.isValidCoordinate(newRow, newCol)) {
+        while (BoardUtils.isValidCoordinate(newRow, newCol)){
             int[] destinationCoord = {newRow, newCol};
             Tile destinationTile = board.getTile(destinationCoord);
-            if (destinationTile.isEmpty()) {
+            if (destinationTile.isEmpty()){
                 Move toAdd = new Move(board, this, destinationCoord);
                 set.add(toAdd);
                 newRow += rowOffset;
                 newCol += colOffset;
             } else {
                 Piece potentialVictim = destinationTile.getPiece();
-                if (potentialVictim.getTeam() != this.getTeam()) {
+                if (potentialVictim.getTeam() != this.getTeam()){
                     AttackMove toAdd = new AttackMove(board, this, destinationCoord, potentialVictim);
-                    set.add(toAdd);
                 }
                 break;
             }
         }
     }
-}
 
+
+}
