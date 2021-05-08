bb8eda2b1a1446f15443e66780601bace240333e
Shoumik Chaudhuri <shoumik.c@berkeley.edu>
2021-05-07 19:57:29 -0700

    working on Board, created BoardBuilder

--- src/all/chess/board/Board.java
+++ src/all/chess/board/Board.java
@@ -1,6 +1,34 @@
 package all.chess.board;
 
+import all.chess.Color;
+import all.chess.pieces.Piece;
+
 public class Board {
+    public static class BoardBuilder{
+        Color nextMover;
+        Piece[][] boardLayout = new Piece[8][8];
+        public BoardBuilder(){}
+        public BoardBuilder setPiece(Piece piece){
+            int row = piece.getRow();
+            int col = piece.getCol();
+            boardLayout[row][col] = piece;
+            return this;
+        }
+        public BoardBuilder setNextMover(Color team){
+            nextMover = team;
+            return this;
+        }
+        public Board build(){
+            return new Board(this);
+        }
+    }
+
+    private final Tile[][] board = new Tile[8][8];
+    private Board(BoardBuilder builder){
+
+
+    }
+
     public Tile getTile(int[] coordinate){
         //TODO:
         return null;
--- src/all/chess/pieces/Piece.java
+++ src/all/chess/pieces/Piece.java
@@ -21,12 +21,16 @@ public abstract class Piece {
         this.col = BoardUtils.getCol(this.coordinate);
     }
 
-
     /** Returns hashset of legal moves from current board */
     public abstract HashSet<Move> legalMoves(Board board);
+    /** Returns coordinates of piece */
+    public int[] getCoordinate(){return coordinate;}
+    /** Returns row coord of piece */
+    public int getRow() {return row;}
+    /** Returns col coord of piece */
+    public int getCol() {return col;}
     /** Returns color of piece */
-    public Color getTeam(){
-        return this.team;
-    }
+    public Color getTeam(){return team;}
+
     
 }
